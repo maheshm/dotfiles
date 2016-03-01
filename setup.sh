6e785c0e3bdd0adcf041d35b28a5912af86e8b3a
Mahesh M <meshy@grassious.com>
2016-03-02 00:17:26 +0530

    First stab

---
+++ setup.sh
@@ -0,0 +1,8 @@
+rm -f $HOME/.aliases
+ln -s `pwd`/dot_aliases $HOME/.aliases
+
+rm -f $HOME/.gitconfig
+ln -s `pwd`/dot_gitconfig $HOME/.gitconfig
+
+rm -f $HOME/.zshrc
+ln -s `pwd`/dot_zshrc $HOME/.zshrc
